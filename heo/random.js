cd08cc159ccba252aad4ab3df2fd467239878630
timigomen <121149519+timigomen@users.noreply.github.com>
2023-03-05 15:29:01 +0800

    Site updated: 2023-03-05 15:29:01

--- heo/random.js
+++ heo/random.js
@@ -1,1 +1,1 @@
-var posts=["posts/46819/","posts/8405/","posts/55021/","posts/49736/","posts/41694/","posts/55088/","posts/20230304/","posts/4316/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["posts/46819/","posts/55021/","posts/8405/","posts/55088/","posts/20230304/","posts/49736/","posts/41694/","posts/4316/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
